efe27d2f943405acbe984b52c63189cad5d36cd4
Aryan Kumar <aryanak9163@gmail.com>
2023-01-01 20:14:20 +0530

    created schema for DB and created models

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Available routes
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello aryan!");
 });
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
